a0683a6b741df94947dd1bea558a314daf0dea3f
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2023-01-05 13:55:45 +0100

    added Websites.Razor.ClassLibrary

---
+++ Websites.Razor.ClassLibrary/Components/LanguageSelector.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Websites.Razor.ClassLibrary.Components
+{
+	public class LanguageSelectorBase: ComponentBase
+	{
+		protected string SelectedCountryCode = "GB";
+
+		protected const string CountryCodeUk = "GB";
+		protected const string CountryCodeGermany = "DE";
+		protected const string CountryCodeItaly = "IT";
+
+		protected void OnChange(ChangeEventArgs e)
+		{
+
+		}
+
+		protected void OnClick(MouseEventArgs e)
+		{
+
+		}
+
+		protected void OnSelect(EventArgs e)
+		{
+
+		}
+
+		protected void OnClickItem(MouseEventArgs e)
+		{
+
+		}
+	}
+}
