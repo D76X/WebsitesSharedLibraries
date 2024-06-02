1e5800021e4197cf17f15f444e888061209ede69
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-06-02 19:20:56 +0200

    moved MenuItem component to WSSL

---
+++ Websites.Razor.ClassLibrary/Components/MenuItemBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Routing;
+using Websites.Razor.ClassLibrary.Abstractions.Models;
+
+namespace Websites.Razor.ClassLibrary.Components
+{
+    public class MenuItemBase : NavLink
+    {
+        [Parameter]
+        public IMenuItem? Model { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            /* Basic component layout inheritance in Blazor
+               https://stackoverflow.com/questions/59990832/basic-component-layout-inheritance-blazor
+               
+               // this would be used in the *.razor file
+               base.BuildRenderTree(__builder);
+            */
+
+            //...your own logic...
+
+            // this is the equivalent in the code behind file.
+            base.BuildRenderTree(builder);
+        }
+    }
+}
