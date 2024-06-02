26f97312fadce5e2b565c3be9ebbc74e42bbc391
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-06-02 16:51:39 +0200

    moved CardBase.cs and Card.razore to WSSL

---
+++ Websites.Razor.ClassLibrary/Components/CardBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Websites.Razor.ClassLibrary.Abstractions.Models;
+
+namespace Websites.Razor.ClassLibrary.Components
+{
+    public class CardBase : ComponentBase
+    {
+        [Parameter]
+        public ICardModel? Model { get; set; }
+
+        public string ImageSrc => Model!.ImageSrc;
+        public string PageRef => Model!.PageRef;
+        public string PageTitle => Model!.PageTitle;
+        public string Text => Model!.PageText;
+    }
+}
