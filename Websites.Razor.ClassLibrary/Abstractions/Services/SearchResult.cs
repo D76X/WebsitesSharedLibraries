3623a380c5d728abe1b10729da987db9e5ad9d50
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-06-15 14:41:15 +0200

    improved SearchResult with SearchTerms collection property

--- Websites.Razor.ClassLibrary/Abstractions/Services/SearchResult.cs
+++ Websites.Razor.ClassLibrary/Abstractions/Services/SearchResult.cs
@@ -2,14 +2,14 @@
 
 public class SearchResult
 {
-    public readonly IEnumerable<string> searchTerms;
+    public readonly IEnumerable<string> SearchTerms;
     public readonly IEnumerable<string> Values;
 
     public SearchResult(
         IEnumerable<string> searchTerms, 
         IEnumerable<string> values)
     {
-        this.searchTerms = searchTerms;
+        SearchTerms = searchTerms;
         Values = values;
     }
 }
