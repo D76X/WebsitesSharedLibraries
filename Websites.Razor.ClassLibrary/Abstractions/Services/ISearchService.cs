3d21ecfeff911a1093874a4d328222d68300d41c
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-06-15 14:40:24 +0200

    WSSL ISearchService event handling plumbing SearchResultsChanged

--- Websites.Razor.ClassLibrary/Abstractions/Services/ISearchService.cs
+++ Websites.Razor.ClassLibrary/Abstractions/Services/ISearchService.cs
@@ -2,6 +2,9 @@
 
 public interface ISearchService
 {
+    event EventHandler<IEnumerable<SearchResult>>? SearchResultsChanged;
+
     //IEnumerable<SearchResult> GetResults(IEnumerable<string> searchTerms);
     IEnumerable<SearchResult> GetResults(string searchTerm);
+    IEnumerable<SearchResult> SearchResults { get; }
 }
