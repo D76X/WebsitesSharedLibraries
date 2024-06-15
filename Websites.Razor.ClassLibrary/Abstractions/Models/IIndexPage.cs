1e23c327e8e3721f6662186ff07dcadc236af4b7
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-06-15 14:38:27 +0200

    WSSL added IIndexPage.OnStateHasChanged and tempory change TestString

--- Websites.Razor.ClassLibrary/Abstractions/Models/IIndexPage.cs
+++ Websites.Razor.ClassLibrary/Abstractions/Models/IIndexPage.cs
@@ -2,5 +2,7 @@
 
 public interface IIndexPage: IDisposable
 {
+    public event Action? OnStateHasChanged;
     public ICardModel GetCard(string cardId);
+    public string TestString { get; set; }
 }
