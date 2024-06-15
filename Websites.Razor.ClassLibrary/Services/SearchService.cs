7575888f56260fc216ad6f95a6fc44761a8da973
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-06-15 14:42:52 +0200

    WSSL basic implementation SearchServicen event handling SearchResultsChanged

--- Websites.Razor.ClassLibrary/Services/SearchService.cs
+++ Websites.Razor.ClassLibrary/Services/SearchService.cs
@@ -6,18 +6,35 @@ namespace Websites.Razor.ClassLibrary.Services
     {
         public static string? SearchTerm = null;
 
+        private IEnumerable<SearchResult>? _searchResults;
+        
+        public event EventHandler<IEnumerable<SearchResult>>? SearchResultsChanged;
+        protected virtual void OnSearchResults(IEnumerable<SearchResult> e) => 
+            SearchResultsChanged?.Invoke(this, e);
+
+        public IEnumerable<SearchResult> SearchResults
+        {
+            get => _searchResults;
+
+            set
+            {
+                _searchResults = value;
+                OnSearchResults(value);
+            }
+        }
+
         public IEnumerable<SearchResult> GetResults(string searchTerm)
         {
             var searchResults = new List<SearchResult>();
             var searchResult = new SearchResult(new[] { searchTerm }, new[] { "found!" });
             searchResults.Add(searchResult);
+            SearchResults = searchResults;
             return searchResults;
         }
 
         public IEnumerable<SearchResult> GetResults(IEnumerable<string> searchTerms)
         {
             throw new NotImplementedException();
-
         }
     }
 }
