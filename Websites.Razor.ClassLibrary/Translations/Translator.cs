5a25f52d3e263cc11121c0274928c64551f022a3
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-10-07 11:17:47 +0200

    WSL moved the Translator class to this shared library

---
+++ Websites.Razor.ClassLibrary/Translations/Translator.cs
@@ -0,0 +1,27 @@
+﻿using Websites.Razor.ClassLibrary.Components;
+
+namespace Websites.Razor.ClassLibrary.Translations
+{
+    public static class Translator
+    {
+        public static string Get(
+            string id,
+            string language,
+            Dictionary<string, string[]> translations)
+        {
+            int languageIndex;
+
+            switch (language)
+            {
+                case LanguageSelectorBase.LanguageEn: languageIndex = 0; break;
+                case LanguageSelectorBase.LanguageDe: languageIndex = 1; break;
+                case LanguageSelectorBase.LanguageIt: languageIndex = 2; break;
+                default: languageIndex = 0; break;
+
+            }
+
+            if (translations.TryGetValue(id, out var values)) return values[languageIndex];
+            return @"translation error";
+        }
+    }
+}
