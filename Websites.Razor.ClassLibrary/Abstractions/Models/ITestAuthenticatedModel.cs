3c24eb0ed8a838173eedd4c7fde8c522b9d31795
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-12-14 13:54:55 +0100

    WSL ITestAuthenticatedModel + ITestAuthorizedModel

---
+++ Websites.Razor.ClassLibrary/Abstractions/Models/ITestAuthenticatedModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Websites.Razor.ClassLibrary.Abstractions.Models;
+
+public interface ITestAuthenticatedModel : IDisposable
+{
+    public event Action OnStateHasChanged;
+    string SelectedLanguage { get; }
+    bool IsEnglish { get; }
+    bool IsDeutsch { get; }
+    bool IsItalian { get; }
+}
+
+public interface ITestAuthorizedModel : IDisposable
+{
+    public event Action OnStateHasChanged;
+    string SelectedLanguage { get; }
+    bool IsEnglish { get; }
+    bool IsDeutsch { get; }
+    bool IsItalian { get; }
+}
