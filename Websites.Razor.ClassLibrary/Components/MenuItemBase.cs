e78ccd661f3f0d2d95a5fe1cfc281cd4e88da9b9
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2025-01-25 10:10:59 +0100

    WSL to file scoped namespace

--- Websites.Razor.ClassLibrary/Components/MenuItemBase.cs
+++ Websites.Razor.ClassLibrary/Components/MenuItemBase.cs
@@ -3,26 +3,25 @@ using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.AspNetCore.Components.Routing;
 using Websites.Razor.ClassLibrary.Abstractions.Models;
 
-namespace Websites.Razor.ClassLibrary.Components
+namespace Websites.Razor.ClassLibrary.Components;
+
+public class MenuItemBase : NavLink
 {
-    public class MenuItemBase : NavLink
+    [Parameter]
+    public IMenuItem? Model { get; set; }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
-        [Parameter]
-        public IMenuItem? Model { get; set; }
+        /* Basic component layout inheritance in Blazor
+           https://stackoverflow.com/questions/59990832/basic-component-layout-inheritance-blazor
+           
+           // this would be used in the *.razor file
+           base.BuildRenderTree(__builder);
+        */
 
-        protected override void BuildRenderTree(RenderTreeBuilder builder)
-        {
-            /* Basic component layout inheritance in Blazor
-               https://stackoverflow.com/questions/59990832/basic-component-layout-inheritance-blazor
-               
-               // this would be used in the *.razor file
-               base.BuildRenderTree(__builder);
-            */
+        //...your own logic...
 
-            //...your own logic...
-
-            // this is the equivalent in the code behind file.
-            base.BuildRenderTree(builder);
-        }
+        // this is the equivalent in the code behind file.
+        base.BuildRenderTree(builder);
     }
 }
